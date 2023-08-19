b5a75d61b726b9876f1c3d023955b0bc38e88f8f
mandeepkaur86 <mandeepkaurdia@gmail.com>
2023-08-19 16:16:31 -0700

    back up before moving component in src

--- GitHubCard/GitHubCard.jsx
+++ GitHubCard/GitHubCard.jsx
@@ -1,11 +1,13 @@
 import "./GitHubCard.css";
 import React from "react";
+import Image from "react-bootstrap/Image";
 
 function template() {
   return (
     <div className="git-hub-card">
       <h1>Mandeep Kaur</h1>
       <h2>About Me</h2>
+      <Image src ="src/Image/mandeep.jpg"></Image>
     </div>
   );
 };
