4fe63f6eab376892d3e211d9a28b47ab5723d0bc
mandeepkaur86 <mandeepkaurdia@gmail.com>
2023-08-19 16:03:32 -0700

    2nd test pass after adding card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import GitHubCard from '../GitHubCard';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
         <p>more code here. some more code.add something more.more and more</p>
+        <GitHubCard>My Info</GitHubCard>
       </header>
     </div>
   );
